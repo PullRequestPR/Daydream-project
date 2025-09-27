cc52899805f9885c26868a63d1ba3489c1948bb0
Poshkin Omer <omerposhkin31@gmail.com>
2025-09-27 11:59:52 -0700

    feat: add W,A,S,D keys

--- player.gd
+++ player.gd
@@ -7,14 +7,14 @@ const GRAVITY = 900
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("move_right"):
 		velocity.x = SPEED
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_left") or Input.is_action_pressed("move_left"):
 		velocity.x = -SPEED
 	else:
 		velocity.x = 0
 
-	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
+	if is_on_floor() and (Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("jump")):
 		velocity.y = JUMP_FORCE
 
 	move_and_slide()
