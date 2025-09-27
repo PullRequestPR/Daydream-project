dbc7000ac01d15f124605254d87d73ea01d3c853
Poshkin Omer <omerposhkin31@gmail.com>
2025-09-27 16:09:10 -0700

    feat: fix animation and floor

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @onready var sfx_jump: AudioStreamPlayer = $sfx_jump
-
+@onready var anim_sprite = $Sprite2D
 
 const SPEED = 300
 const JUMP_FORCE = -400
@@ -16,11 +16,14 @@ func _physics_process(delta):
 
 	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("move_right"):
 		velocity.x = SPEED
+		anim_sprite.play("walking")
 	elif Input.is_action_pressed("ui_left") or Input.is_action_pressed("move_left"):
 		velocity.x = -SPEED
+		
+		anim_sprite.play("walking reverse")
 	else:
 		velocity.x = 0
-
+		anim_sprite.play("idle")
 	if (Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("jump")) and jump_count < MAX_JUMPS:
 		velocity.y = JUMP_FORCE
 		jump_count += 1
